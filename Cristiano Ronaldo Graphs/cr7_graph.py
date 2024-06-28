dfea08a232c2e1c5d833ebe69f25960b546f3faa
BerattCelikk <138959468+BerattCelikk@users.noreply.github.com>
2024-06-28 14:20:45 +0300

    Update cr7_graph.py

--- Cristiano Ronaldo Graphs/cr7_graph.py
+++ Cristiano Ronaldo Graphs/cr7_graph.py
@@ -10,10 +10,10 @@ colors2 = ["g", "r", "y"]
 
 fig,(ax1, ax2) = plt.subplots(1, 2,figsize=(14, 7))
 
-ax1.pie(amount_of_scores, labels=[f"{score_types[i]} ({amount_of_scores[i]})" for i in range(len(score_types))], colors=colors1, autopct="%1.1f%%", startangle=90)
+ax1.pie(amount_of_scores, labels=[f"{score_types[i]} ({amount_of_scores[i]})" for i in range(len(score_types))], colors=colors1, autopct="%1.1f%%", startangle=90,shadow=True,explode=(0.05,0.05,0.05,0.05,0.05,0.05))
 ax1.set_title("Score Types and Amounts")
 
-ax2.pie(amount_of_matches, labels=[f"{match_types[i]} ({amount_of_matches[i]})" for i in range(len(match_types))], colors=colors2, autopct="%1.1f%%", startangle=90)
+ax2.pie(amount_of_matches, labels=[f"{match_types[i]} ({amount_of_matches[i]})" for i in range(len(match_types))], colors=colors2, autopct="%1.1f%%", startangle=90,shadow=True,explode=(0.05,0.05,0.05))
 ax2.set_title("Total Matches (All Season)")
 
 plt.tight_layout()
